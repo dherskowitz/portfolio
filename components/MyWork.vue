2af052f0bb2fa1fea3be1355b687eafc2b2ac93c
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-12-02 16:47:23 +0200

    new component for toggling in mobile

---
+++ components/MyWork.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="col-span-6 md:col-span-4 px-6 items-center border-gray-800 border-2 justify-center">
+      <div>My Work</div>
+      <div>A link to a page with a list of public work.</div>
+    </div>
+</template>
