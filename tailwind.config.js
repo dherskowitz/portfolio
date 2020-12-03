17c795104c2368e7be57de2c3bfc1afc7afe526d
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-12-03 11:21:02 +0200

    add container to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     purge: [],
     theme: {
       extend: {},
+      container: {
+        center: true,
+        padding: "1rem",
+      },
     },
     variants: {},
     plugins: [],
