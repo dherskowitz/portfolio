4a13fbf450b32006f39fb58ac7f9393b70f8f95c
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-12-03 13:43:31 +0200

    change hover effect

--- pages/index.vue
+++ pages/index.vue
@@ -14,13 +14,13 @@
     </div>
 
     <div class="col-span-3 md:col-span-1 p-6 flex items-center justify-center border-gray-800 border-2">
-      <a href="https://www.linkedin.com/in/daniel-herskowitz-105/" target="_blank" class="inline-block bg-green-900 text-white px-4 py-2 shadow-md hover:scale-110 hover:rotate-6 transition-all transform duration-200">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/daniel-herskowitz-105/" target="_blank" class="inline-block bg-green-900 text-white px-4 py-2 hover:shadow-xl hover:bg-green-700 transition-all duration-150 ease-in-out">LinkedIn</a>
     </div>
 
     <MyWork class="hidden flex-col md:flex" />
 
     <div class="col-span-3 md:col-span-1 p-6 flex items-center justify-center border-gray-800 border-2">
-      <a href="https://github.com/dherskowitz" target="_blank" class="inline-block bg-green-900 text-white px-4 py-2 shadow-md hover:scale-110 hover:rotate-6 transition-all transform duration-200">Github</a>
+      <a href="https://github.com/dherskowitz" target="_blank" class="inline-block bg-green-900 text-white px-4 py-2 hover:shadow-xl hover:bg-green-700 transition-all duration-150 ease-in-out">Github</a>
     </div>
 
     <MyWork class="flex flex-col md:hidden" />
@@ -28,12 +28,12 @@
     <div class="col-span-6 md:col-span-3 p-6 flex flex-col items-center border-gray-800 border-2 justify-center text-center">
       <!-- <div class="text-4xl text-gray-900 mb-1">About Me</div> -->
       <div class="text-2xl lg:text-4xl text-gray-900 mb-4 capitalize">My history & experience</div>
-      <a href="/aboutme" class="inline-block bg-green-900 text-white px-4 py-2 shadow-md hover:scale-110 hover:rotate-6 transition-all transform duration-200">About Me</a>
+      <a href="/aboutme" class="inline-block bg-green-900 text-white px-4 py-2 hover:shadow-xl hover:bg-green-700 transition-all duration-150 ease-in-out">About Me</a>
     </div>
 
     <div class="col-span-6 md:col-span-3 p-6 flex flex-col items-center border-gray-800 border-2 justify-center text-center">
       <p class="text-2xl lg:text-4xl text-gray-900 mb-4 capitalize">My Resume</p>
-      <a href="/Daniel Herskowitz Resume.pdf" target="_blank" class="inline-block bg-green-900 text-white px-4 py-2 shadow-md hover:scale-110 hover:rotate-6 transition-all transform duration-200 capitalize">Download my resume as a PDF</a>
+      <a href="/Daniel Herskowitz Resume.pdf" target="_blank" class="inline-block bg-green-900 text-white px-4 py-2 hover:shadow-xl hover:bg-green-700 transition-all duration-150 ease-in-out capitalize">Download my resume as a PDF</a>
     </div>
   </div>
 </template>
